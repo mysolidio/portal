2902e2595917147d1d476dd5fc3e19d17caf0dbe
haihv <haihv27@gmail.com>
2025-03-17 22:13:34 +0700

    Fix book-demo button

--- components/header/menu.tsx
+++ components/header/menu.tsx
@@ -56,7 +56,7 @@ const BookDemoButton: typeof Button = ({ className, children, ...props }) => {
       variant="special"
       {...props}
       className={cn(
-        "rounded-full bg-white bg-linear-0 text-black uppercase hover:text-white",
+        "h-15 rounded-full bg-white bg-linear-0 text-black uppercase hover:text-white",
         className,
       )}
     >

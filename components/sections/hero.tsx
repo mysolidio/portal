00fa0c95e3df0f80c6b9bd800058362ac765e070
hoangtuanictvn <19290570+hoangtuanictvn@users.noreply.github.com>
2025-03-25 19:06:27 +0700

    chore: redeploy

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -5,6 +5,12 @@ import { ArrowUpRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export default function HeroSection() {
+  const jumpToBookDemo = () => {
+    if (window) {
+      window.open("https://forms.gle/ax3Ds57nmMHQY97d7", "_blank");
+    }
+  };
+
   return (
     <div className="relative grid h-screen w-full place-content-center overflow-hidden bg-[#001024]">
       <div className="morphing-gradient-1 absolute -top-4 -right-30 h-60 w-70 rotate-6 rounded-full bg-gradient-to-r from-indigo-800 via-purple-900 to-pink-700 opacity-50"></div>
@@ -22,15 +28,15 @@ export default function HeroSection() {
       </div>
       <div className="absolute inset-0 backdrop-blur-lg" />
       <div className="relative z-10 flex flex-col items-center justify-center px-4 text-center text-white">
-        <div className="text-[2rem] leading-[1.2] text-purple-600 lg:text-4xl">
+        <div className="text-bold text-[3rem] leading-[1.2] lg:text-4xl">
           Universal Digital Identity
           <br className="block" />
         </div>
-        <div className="pt-3 text-[1.5rem] leading-[0.6] lg:text-2xl">
+        <div className="pt-3 text-[2rem] leading-[0.6] lg:text-2xl">
           Verify once, use anywhere
         </div>
-        <div className="mt-4 text-[16px] leading-[1.3] lg:text-[20px]">
-          Solid provides a global standard for digital identity and{" "}
+        <div className="mt-4 text-[16px] leading-[1.3] text-gray-200 lg:text-[20px]">
+          A global standard for digital identity and{" "}
           <br className="block md:hidden" />
           data self sovereignty
         </div>
@@ -47,12 +53,7 @@ export default function HeroSection() {
             size="xl"
             variant="ghost"
             className="h-15 gap-2.5 rounded-full !px-8 uppercase transition-all duration-300 ease-in-out"
-            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-            onClick={() => {
-              window
-                ? window.open("https://forms.gle/ax3Ds57nmMHQY97d7", "_blank")
-                : null;
-            }}
+            onClick={jumpToBookDemo}
           >
             Book a demo
           </Button>

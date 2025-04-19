94178d21c8643e142b8b102dfd5848f9e88ad7ed
haihv <haihv27@gmail.com>
2025-04-19 21:49:21 +0700

    Update services section

--- components/sections/services.tsx
+++ components/sections/services.tsx
@@ -4,6 +4,7 @@ import {
   DataScientistIcon,
   FraudDetectionIcon,
 } from "@/icons";
+import { cn } from "@/lib/utils";
 
 const items = [
   {
@@ -30,31 +31,35 @@ const items = [
 
 export default function ServicesSection() {
   return (
-    <div className="bg-[#001024] py-20 text-white lg:py-30">
+    <div className="bg-black py-20 text-white">
       <div className="container space-y-15">
         <div className="mx-auto max-w-2xl lg:mx-0 [&>*]:text-center lg:[&>*]:text-left">
-          <div className="text-[12px] leading-[1.3] font-bold tracking-[6px] uppercase lg:text-[16px]">
+          <div className="text-xs font-bold tracking-[18px] uppercase lg:text-xl lg:font-light">
             Services
           </div>
-          <div className="mt-2 text-[32px] leading-[1.2] font-semibold lg:mt-4 lg:text-[42px]">
+          <div className="mt-8 text-[32px] leading-[1.2] font-normal lg:mt-4 lg:text-[58px] lg:font-light">
             Your gateway to web3
           </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:gap-x-12 lg:gap-y-10">
+        <div className="grid grid-cols-1 gap-2.5 md:grid-cols-2 md:gap-x-10 md:gap-y-6 lg:gap-x-[72px]">
           {items.map(({ title, description, Icon }, idx) => (
             <div
               key={idx}
-              className="flex items-start gap-6 rounded-md border border-[#0a2452] bg-[#001838]/50 px-5 py-6 transition-colors duration-300 hover:border-[#1a3a6c]"
+              className={cn(
+                "relative z-1 rounded-[21px] border border-transparent bg-[#151513] p-7 lg:space-y-8",
+                "before:absolute before:inset-0 before:-z-[1] before:rounded-[21px]",
+                "before:bg-[linear-gradient(118.71deg,_#FFFFFF_11.96%,_#212121_45.04%,_#BDBDBD_66.69%,_#232323_75.79%,_#646464_86.15%)]",
+                "after:absolute after:inset-[1px] after:z-[-1] after:rounded-[20px] after:bg-[#151513]",
+                "space-y-[42px]",
+              )}
             >
-              <div className="rounded-md bg-[#00256c] p-3">
-                <Icon className="size-[40px] shrink-0 text-white" />
-              </div>
+              <Icon className="size-[65px] shrink-0 lg:size-[100px]" />
               <div className="space-y-2">
-                <div className="text-[20px] leading-[1.3] font-semibold text-white">
+                <div className="text-[25px] leading-[1.2] font-normal text-white lg:text-[32px]">
                   {title}
                 </div>
-                <div className="text-[14px] leading-[1.4] text-white opacity-75">
+                <div className="text-sm text-white opacity-60">
                   {description}
                 </div>
               </div>

9a5404209f6fc24e431f946c98bfac1e9a764967
haihv <haihv27@gmail.com>
2025-04-20 20:59:46 +0700

    Update Hero

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,15 +1,18 @@
 import DemoButton from "@/common/demoButton";
-import SolidChrome from "@/common/solidChrome";
 
 export default function HeroSection() {
   return (
-    <section className="mt-20 bg-black pt-14 pb-20 text-center text-white lg:pt-20">
-      <SolidChrome className="mx-auto h-[130px] lg:h-[200px]" />
-      <div className="mt-8 text-[32px] font-light tracking-[0.3px] lg:text-6xl">
-        Verify Once, Use Anywhere
+    <section className="mt-20 bg-white pt-14 pb-5 text-center text-black lg:pt-10">
+      <div className="align-middle text-[90px] leading-[136px] font-medium -tracking-[5.63px] text-[#373737] lg:text-[150px] lg:-tracking-[9.63px]">
+        Solid
       </div>
-      <div className="mt-2.5 text-base font-light lg:mt-4 lg:text-[23px]">
-        A global standard for digital identity and data self sovereignty.
+      <div className="mt-8 align-middle text-[32px] leading-[30px] font-light tracking-[0.3px] md:mt-14 md:text-[40px]">
+        Verify Once. <br className="md:hidden" />
+        Use Anywhere.
+      </div>
+      <div className="mt-6 text-base font-light lg:mt-7 lg:text-[20px]">
+        A global standard for digital identity <br className="md:hidden" /> and
+        data self sovereignty.
       </div>
       <DemoButton className="mt-16 h-16 px-11 text-lg font-bold lg:mt-20" />
     </section>
